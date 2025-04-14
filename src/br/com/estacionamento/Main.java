dd46077b7654178395a80d49632414a0ed19d1fe
Aline Pinho <alineveira2@gmail.com>
2025-04-14 19:57:08 -0300

    modificando os arquivos dessa pasta

--- src/br/com/estacionamento/Main.java
+++ src/br/com/estacionamento/Main.java
@@ -1,4 +1,16 @@
 package br.com.estacionamento;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Semaphore semaforo = new Semaphore(10);
+        System.out.println("Núm. de vagas livres: " + semaforo.availablePermits());
+
+        for(int i = 0; i < 20;i++) {
+            new Carro("Carro " + (i+1), semaforo).start();
+        }
+    }
+
 }
